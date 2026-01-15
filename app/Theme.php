b02ed3f6d82def255bfb303ce0e831c7f876b0f2
tmw-mm <toby@mirrorandmountain.com>
2026-01-15 00:31:29 +0000

    Format Theme class

--- app/Theme.php
+++ app/Theme.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
 namespace App;
 
 use MM\Meros\Contracts\ThemeManager;
 
-class Theme extends ThemeManager
-{
-    protected function configure(): void
-    {
-        
+class Theme extends ThemeManager {
+
+    protected function configure(): void {
+        // Custom theme configuration can be added here
     }
+
 }
